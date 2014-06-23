ff3185a57844a9af5b11fdd842dcf96592273ed8
Phil Myers <phil@mbp.home>
2014-06-22 22:02:26 -0400

    Finished Chapter 1

--- chapter_1/section3.scm
+++ chapter_1/section3.scm
@@ -40,7 +40,7 @@
     (if (> a b)
       result
       (iter (next a) (* result (term a)))))
-  (iter a 1)))
+  (iter a 1))
 
 ; define factorial using product
 (define (prod-fact n)
@@ -128,6 +128,7 @@
 
 (define tolerance 0.00001)
 (define (fixed-point f first-guess)
+  (define tolerance 0.00001)
   (define (close-enough? v1 v2)
     (< (abs (- v1 v2)) tolerance))
   (define (try guess)
@@ -214,3 +215,148 @@
   (cont-frac (lambda (i) (if (= i 1) x (* -1.0 (square x))))
              (lambda (i) (- (* i 2) 1))
              k))
+
+; Section 1.3.4 Procedures as returned values
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+(define (sqrt x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
+; Newton's Method
+; f(x) = x - g(x)/g'(x) when g(x) is differentiable
+; g'(x) = (g(x + dx) - g(x))/dx as dx -> 0
+(define (deriv g)
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
+(define dx 0.00001)
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+(define (sqrt x)
+  (newtons-method (lambda (y) (- (square y) x))
+                  1.0))
+
+; Exercise 1.40
+; Define a procedure cubic that can be used together with the newtons-method 
+; procedure in expressions of the form
+; (newtons-method (cubic a b c) 1)
+; to approximate zeros of the cubic x3 + ax2 + bx + c.
+(define (cube x)
+  (* x x x))
+(define (cubic a b c)
+  (lambda (x) 
+    (+ (cube x) (* a (square x)) (* b x) c))) 
+(newtons-method (cubic 2 3 6) 1.0)
+; Value: -2
+
+; Exercise 1.41
+; Define a procedure double that takes a procedure of one argument as argument 
+; and returns a procedure that applies the original procedure twice. For 
+; example, if inc is a procedure that adds 1 to its argument, then (double inc)
+; should be a procedure that adds 2. What value is returned by
+; (((double (double double)) inc) 5)
+(define (inc n) (+ n 1))
+(define (double f)
+  (lambda (x) (f (f x))))
+(((double (double double)) inc) 5)
+; 21 - inc is expanded and applied 16 times
+
+; Exercise 1.42  
+; Let f and g be two one-argument functions. The composition f after g is 
+; defined to be the function x  f(g(x)). Define a procedure compose that  
+; implements composition. For example, if inc is a procedure that adds 1 to its
+; argument,
+((compose square inc) 6)
+; 49
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+; Exercise 1.43  
+; Write a procedure that takes as inputs a procedure that computes f and a 
+; positive integer n and returns the procedure that computes the nth repeated 
+; application of f. Your procedure should be able to be used as follows:
+((repeated square 2) 5)
+625
+(define (repeated f n)
+  (define (repeated-inner g n)
+    (if (= n 1) 
+      g
+      (repeated-inner (compose g f) (- n 1))))
+  (repeated-inner f n))
+
+; Exercise 1.44
+; The idea of smoothing a function is an important concept in signal processing.
+; If f is a function and dx is some small number, then the smoothed version of f
+; is the function whose value at a point x is the average of f(x - dx), f(x), 
+; and f(x + dx). Write a procedure smooth that takes as input a procedure that 
+; computes f and returns a procedure that computes the smoothed f. It is 
+; sometimes valuable to repeatedly smooth a function (that is, smooth the 
+; smoothed function, and so on) to obtained the n-fold smoothed function. Show 
+; how to generate the n-fold smoothed function of any given function using 
+; smooth and repeated from exercise 1.43.
+(define dx 0.0001)
+(define (smooth f)
+  (lambda (x)
+    (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3)))
+(define (n-fold-smooth f n)
+  ((repeated smooth n) f))
+
+; Exercise 1.45 
+; Do some experiments to determine how many average damps are required to 
+; compute nth roots as a fixed-point search based upon repeated average damping
+; of y  x/yn-1. Use this to implement a simple procedure for computing nth 
+; roots using fixed-point, average-damp, and the repeated procedure of exercise
+; 1.43. Assume that any arithmetic operations you need are available as 
+; primitives.
+(define (exp b n)
+  (define (succ-sqr a b n)
+    (cond ((= n 0) a)
+          ((even? n) (succ-sqr a (square b) (/ n 2)))
+          (else (succ-sqr (* a b) b (- n 1)))))
+  (succ-sqr 1 b n))
+(define (log2 x) (/ (log x) (log 2)))
+(define (nth-root n x)
+  (fixed-point ((repeated average-damp (floor (log2 n)))
+                (lambda (y) (/ x (exp y (- n 1)))))
+                1.0))
+
+; Exercise 1.46
+; Write a procedure iterative-improve that takes two procedures as arguments: a
+; method for telling whether a guess is good enough and a method for improving a
+; guess. Iterative-improve should return as its value a procedure that takes a
+; guess as argument and keeps improving the guess until it is good enough. 
+; Rewrite the sqrt procedure of section 1.1.7 and the fixed-point procedure of 
+; section 1.3.3 in terms of iterative-improve.
+(define (iterative-improve good-enough? improve)
+  (lambda (x)
+    (define (inner-improve x)
+      (let ((improved-x (improve x)))
+        (if (good-enough? x improved-x)
+          improved-x
+          (inner-improve improved-x))))
+    (inner-improve x)))
+(define tolerance 0.00001)
+(define (close-enough? v1 v2)
+  (< (abs (- v1 v2)) tolerance))
+(define (sqrt x)
+  ((iterative-improve close-enough? 
+                      (lambda (y) (/ (+ (/ x y) y) 2))) 1.0))
+(define (fixed-point f guess)
+  ((iterative-improve close-enough? f) guess))
